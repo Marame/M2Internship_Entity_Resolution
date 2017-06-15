e609a4f56e94bf2c626325e5deb796ad0f9c8586
romdhane <maramromdhane@gmail.com>
2017-06-15 09:50:19 +0200

    EvaluationVSM

---
+++ src/Document.java
@@ -0,0 +1,53 @@
+/**
+ * Created by romdhane on 13/06/17.
+ */
+public class Document {
+
+    private String id ;
+    private String name;
+    private Double score;
+
+    public Document() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Document(String id, String name, Double score) {
+        this.id =id;
+
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
