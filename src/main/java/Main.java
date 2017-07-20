de86c3df13d69002e1c0cdcc7594770c7a756200
romdhane <maramromdhane@gmail.com>
2017-07-20 23:04:47 +0200

    "format fix"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,14 +19,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Main main = new Main();
+        //main.startTesting(args[0], args[1], args[2], args[3], args[4], args[5]);
         main.startTesting(args[0], args[1]);
     }
 
 
+    // public void startTesting(String filenameQueries, String filenameDocs, String predictFile, int ngram , double k) throws FileNotFoundException, IOException {
     public void startTesting(String filenameQueries, String filenameDocs) throws FileNotFoundException, IOException {
 
-
-            //Our initial Evaluation entities
+        //Our initial Evaluation entities
            /* for (EvaluationEntity e : ee) {
                 System.out.println("query: " + e.getQuery().getName());
                 int i = 1;
@@ -35,47 +36,77 @@ public class Main {
                     i++;
                 }
             }*/
-            ParseFiles pf = new ParseFiles();
-            List<EvaluationEntity> ee = pf.parseArgs(filenameQueries, filenameDocs);
-            System.out.println("$$$$$$$$$$$$ Results for Vector Space Model $$$$$$$$$$$$$");
+        ParseFiles pf = new ParseFiles();
+        List<EvaluationEntity> ee = pf.parseArgs(filenameQueries, filenameDocs);
+        System.out.println("$$$$$$$$$$$$ Results for Vector Space Model $$$$$$$$$$$$$");
 
-            StringBuilder sb = new StringBuilder();
-            sb.append(String.format("%-10s%-20s%-20s%-20s%\n", "version", "MAP:nothing", "MAP:Stemming", "MAP:Lemmatizing" + "\n"));
-            sb.append(String.format("============================================================\n"));
-            for (String version : vsm_versions) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%-10s%-20s%-20s%-20s", "version", "MAP:nothing", "MAP:Stemming", "MAP:Lemmatizing" + "\n"));
+        sb.append(String.format("============================================================\n"));
+        for (String version : vsm_versions) {
 
-                Evaluation eval = new Evaluation();
-                eval.final_evaluation(ee, "", version, "nothing");
+            Evaluation eval = new Evaluation();
+            eval.final_evaluation(ee, "", version, "nothing");
 
-                Evaluation eval1 = new Evaluation();
-                eval1.final_evaluation(ee, "", version, "stemming");
-                Evaluation eval2 = new Evaluation();
-                eval2.final_evaluation(ee, "", version, "lemmatizing");
-                sb.append(String.format("%-10s%-20s%-20s%-20s", version, eval.getMAP() + "||", eval1.getMAP() +"||", eval2.getMAP() + "\n"));
-            }
-            System.out.println(sb.toString());
-
-
-            System.out.println("$$$$$$$$$$$$ Results for Language Model $$$$$$$$$$$$$");
-
-            StringBuilder sb1 = new StringBuilder();
-            sb1.append(String.format("%-10s%-30s%-30s%-30s\n", "version", "MAP:nothing", "MAP:Stemming", "MAP:Lemmatizing" + "\n"));
-            sb1.append(String.format("==========================================================\n"));
-
-            for (String version : smoothing_versions) {
-
-                Evaluation eval = new Evaluation();
-                eval.final_evaluation(ee, version, "", "nothing");
-
-                Evaluation eval1 = new Evaluation();
-                eval1.final_evaluation(ee, version, "", "stemming");
-                Evaluation eval2 = new Evaluation();
-                eval2.final_evaluation(ee, version, "", "lemmatizing");
-                sb1.append(String.format("%-20s%-20s%-20s%-20s", version, eval.getMAP() + "||",eval1.getMAP() + "||", eval2.getMAP() + "\n"));
-            }
-            System.out.println(sb1.toString());
+            Evaluation eval1 = new Evaluation();
+            eval1.final_evaluation(ee, "", version, "stemming");
+            Evaluation eval2 = new Evaluation();
+            eval2.final_evaluation(ee, "", version, "lemmatizing");
+            sb.append(String.format("%-10s%-20s%-20s%-20s", version, eval.getMAP() + "||", eval1.getMAP() + "||", eval2.getMAP() + "\n"));
         }
+        System.out.println(sb.toString());
+
+
+        System.out.println("$$$$$$$$$$$$ Results for Language Model $$$$$$$$$$$$$");
+
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(String.format("%-10s%-30s%-30s%-30s\n", "version", "MAP:nothing", "MAP:Stemming", "MAP:Lemmatizing" + "\n"));
+        sb1.append(String.format("==========================================================\n"));
+
+        for (String version : smoothing_versions) {
+
+            Evaluation eval = new Evaluation();
+            eval.final_evaluation(ee, version, "", "nothing");
+
+            Evaluation eval1 = new Evaluation();
+            eval1.final_evaluation(ee, version, "", "stemming");
+            Evaluation eval2 = new Evaluation();
+            eval2.final_evaluation(ee, version, "", "lemmatizing");
+            sb1.append(String.format("%-20s%-20s%-20s%-20s", version, eval.getMAP() + "||", eval1.getMAP() + "||", eval2.getMAP() + "\n"));
+        }
+        System.out.println(sb1.toString());
     }
+}
+
+
+    /*String trainFile = filenameDocs;
+    String predict = predictFile;
+    int nGram = ngram;
+    double K = k;
+    double[] linearInterpoParams;
+		if (args.length > 5) {
+        linearInterpoParams = new double[ngram];
+        for (int i = 0; i < ngram; i++) {
+            linearInterpoParams[i] = Double.parseDouble(args[i+4]);
+        }
+    } else
+    linearInterpoParams = null;
+
+    NGram lm;
+		if (args.length > 4 + ngram) {
+        int lowFreThre = Integer.parseInt(args[ngram+4]), unkThre = Integer.parseInt(args[ngram+5]);
+        lm = new NGram(ngram, K, linearInterpoParams, lowFreThre, unkThre);
+    } else
+    lm = new NGram(ngram, K, linearInterpoParams, 5, 6);
+
+		lm.train(trainFile);
+    Double[] pred = lm.predict(predictFile);
+    String savePred = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+		lm.savePredictions(pred, "/pred-" + savePred + ".txt");
+		lm.evaluate(pred);
+}
+    }*/
+
 
 
             /*for (EvaluationEntity e : ee) {
