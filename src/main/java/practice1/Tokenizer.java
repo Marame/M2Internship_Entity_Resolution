401927f42c830d3e99884ea6dcdd8487b1f25220
romdhane <maramromdhane@gmail.com>
2017-08-07 14:24:32 +0200

    "fix evaluation"

---
+++ src/main/java/practice1/Tokenizer.java
@@ -0,0 +1,44 @@
+package practice1;
+
+import org.tartarus.snowball.ext.PorterStemmer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import static practice1.Index.LEMMATIZING_NLP_METHOD;
+import static practice1.Index.STEMMING_NLP_METHOD;
+
+public class Tokenizer {
+
+    private Lemmatizer lem;
+    private PorterStemmer stemmer;
+
+    public Tokenizer(Lemmatizer lem, PorterStemmer stemmer) {
+        this.lem = lem;
+        this.stemmer = stemmer;
+    }
+
+
+    public List<String> tokenise(String input, String nlp_method) {
+        List<String> tokenised = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(input);
+
+        while (st.hasMoreTokens()) {
+            if (nlp_method.equals(STEMMING_NLP_METHOD)) {
+                stemmer.setCurrent(st.nextToken());
+                stemmer.stem();
+                String result = stemmer.getCurrent();
+                tokenised.add(result);
+            } else if (nlp_method.equals(LEMMATIZING_NLP_METHOD)) {
+                for (String st_lem : lem.lemmatize(st.nextToken())) {
+                    tokenised.add(st_lem);
+                }
+            } else {
+                tokenised.add(st.nextToken());
+            }
+        }
+        return tokenised;
+
+    }
+}
